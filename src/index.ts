712f9d476b41a8be293397c8b496774ece1238ac
Kirill Litvin <litvin.kirill.m@mail.ru>
2022-06-19 22:53:02 +0300

    feat: add start server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import http from 'http';
 import { envConfig } from './common/config';
 
+
 const port = envConfig.SERVER_PORT;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((request, response) => {
+  response.statusCode
+  response.end(); 
 })
+
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`)
 });
+
